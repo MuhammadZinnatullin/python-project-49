321af5cf3ecd91fd97a2b16b6d49e619e65ecdab
MuhammadZinnatullin <zm686917@gmail.com>
2024-06-19 22:43:36 +0300

    Brain games, step 1

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user 
+from brain_games.cli import welcome_user
 
-def greet(who): 
+
+def greet(who):
     print(who)
+
 
 def main():
     greet('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
